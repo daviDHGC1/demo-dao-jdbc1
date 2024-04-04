2ca37181328d270ac7e2b45acdf9b2179540f15d
daviDHGC1 <davidhgc001@gmail.com>
2024-04-04 17:44:10 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Department obj1 = new Department(1, "eletronics");
+		System.out.println(obj1);
+
+	}
+
+}
