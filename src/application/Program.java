3b36f73df66c63ada51eab9f55820ed0cc14be60
daviDHGC1 <davidhgc001@gmail.com>
2024-04-08 12:55:10 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import db.DB;
 import model.dao.impl.SellerDaoJDBC;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -12,12 +15,25 @@ public class Program {
 		SellerDaoJDBC sellerDaoJDBC = new SellerDaoJDBC(DB.getConnection());
 		
 		//Finding by id
+		System.out.println("Finding by Id");
 		seller = sellerDaoJDBC.findById(11);
 		System.out.println(seller);
 		
+		row();
 		
+		/*System.out.println("Finding by Department");
+		//Finding by department
+		Department dep = new Department(10, null);
+		List<Seller> list = sellerDaoJDBC.findByDepartment(dep);
+		for(Seller sellers : list){
+			System.out.println(sellers);
+		}*/
 		
+		List<Seller> list = sellerDaoJDBC.findAll();
+		list.stream().forEach(System.out::println );
 		
 	}
-
+	public static void row() {
+		System.out.println("============================================================");
+	}
 }
